011f777a48211dfd29271d0a3da6279c32650d5d
slobbenmeier <stefan.lobbenmeier@ricoh-europe.com>
2021-10-05 14:54:32 +0200

    Added Service as a class instead of an .aar, does not work yet

--- src/main/java/MockServer.java
+++ src/main/java/MockServer.java
@@ -1,10 +1,18 @@
+import java.util.HashMap;
+
 import org.apache.axis2.context.*;
+import org.apache.axis2.description.*;
+import org.apache.axis2.engine.*;
+import org.apache.axis2.receivers.RawXMLINOutMessageReceiver;
 import org.apache.axis2.transport.http.SimpleHTTPServer;
 
 public class MockServer {
     public static void main(String[] args) throws Exception {
         final int port = 8080;
         final ConfigurationContext configuration = ConfigurationContextFactory.createDefaultConfigurationContext();
+
+        AxisService service = createService(configuration);
+        configuration.deployService(service);
         SimpleHTTPServer receiver = new SimpleHTTPServer(configuration, port);
 
         receiver.start();
@@ -14,5 +22,25 @@ public class MockServer {
         }
         Thread.sleep(1000);
         receiver.stop();
+    }
+
+    private static AxisService createService(ConfigurationContext configuration) throws Exception {
+        AxisConfiguration axisConfig = configuration.getAxisConfiguration();
+
+        HashMap<String, MessageReceiver> messageReceiverMap = new HashMap<>();
+        messageReceiverMap.put(WSDL2Constants.MEP_URI_IN_OUT, new RawXMLINOutMessageReceiver());
+
+        final AxisService service = AxisService.createService(
+                MyService.class.getName(),
+                axisConfig,
+                messageReceiverMap,
+                null,
+                null,
+                MockServer.class.getClassLoader()
+        );
+
+        service.setName("serviceName");
+
+        return service;
     }
 }
